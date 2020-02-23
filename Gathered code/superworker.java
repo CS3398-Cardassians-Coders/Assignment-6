41e34c3a0898ac4a0493185f763cf168ed4a7d18
ZStoebe <60234599+ZStoebe@users.noreply.github.com>
2020-02-23 16:34:40 -0600

    Add files via upload
    by zack

---
+++ Gathered code/superworker.java
@@ -0,0 +1,19 @@
+//Author: Zachary Stoebe
+//Group: Cardassians
+
+
+package threesolid;
+
+class SuperWorker implements IWorker 
+{
+    SuperWorker() {
+    }
+
+    public void work() {
+	System.out.println("I'm working REALLY hard.");
+    }
+
+    public void eat() {
+	System.out.prinltn("I'm eating now.");
+    }
+}
