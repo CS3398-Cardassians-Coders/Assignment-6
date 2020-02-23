01e820a65170c30f18ab955212649731a910e7b8
gaw50 <60453464+gaw50@users.noreply.github.com>
2020-02-23 16:07:39 -0600

    Add files via upload

---
+++ Gathered code/Robot.java
@@ -0,0 +1,15 @@
+//Author: Austin Wallace
+
+package threesolid;
+
+// Class Robot users interface separation and single 
+// Responsibility for Robot workers. They do not 
+// Need to be fed, only work.
+
+class Robot implements Iworkable{
+
+	public void work() {
+
+	}
+
+ 
