fdc0d3e9a7f15310f53bc9c3d8b2380672cde371
jbe38 <60232450+jbe38@users.noreply.github.com>
2020-02-23 14:40:16 -0600

    Add files via upload

---
+++ Gathered code/ThreeSoldInterface.java
@@ -0,0 +1,16 @@
+package threesolid
+
+// Implements the principle of Interface Segregation
+
+interface Iworker extends Iworkable, Ifeedable {}
+
+interface Iworkable
+{
+	public void work();
+}
+
+interface Ifeedable
+{
+	public void feed();
+}
+
