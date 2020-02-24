434dee2c66e74942fd7ef187979608770ecbc752
nsevilla13 <60369589+nsevilla13@users.noreply.github.com>
2020-02-23 21:38:54 -0600

    Add files via upload

---
+++ Gathered code/Worker.java
@@ -0,0 +1,15 @@
+//Author: Nicholas Sevilla
+package threesolid;
+
+//This class was put in a separate folder so it can have a single
+//responsibility and ended it from the interfaces work and feed.
+
+class Worker implements IWorker{
+	public void work() {
+		// ....working
+	}
+
+	public void feed() {
+		//.... eating in launch break
+	}
+}
